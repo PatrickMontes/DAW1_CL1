22721a6837836b2d5a9d870e8fa26b5da4b1c39b
PatrickMontes <alexandermontes30p@hotmail.com>
2024-04-11 12:18:55 -0500

    Creacion del Proyecto

---
+++ src/test/java/pe/edu/cibertec/DawiCl1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
