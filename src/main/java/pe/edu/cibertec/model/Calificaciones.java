70bcffff741966ede5ba67066aee4f7333840d59
PatrickMontes <alexandermontes30p@hotmail.com>
2024-04-11 13:40:12 -0500

    Ejercicio 4 terminado

--- src/main/java/pe/edu/cibertec/model/Calificaciones.java
+++ src/main/java/pe/edu/cibertec/model/Calificaciones.java
@@ -1,2 +1,13 @@
-package pe.edu.cibertec.model;public class Calificaciones {
+package pe.edu.cibertec.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Calificaciones {
+
+    private double nota1;
+    private double nota2;
+    private double nota3;
 }
